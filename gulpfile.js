40f2fe43682877a39df1c9e0c071f4c575d79c3c
Taump <taump@yandex.ru>
2018-12-16 15:17:36 +0700

    add concat and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
 const path = {
     css: {
         input: 'src/css/*.css',
@@ -10,6 +13,7 @@ const path = {
         output: 'build/js'
     }
 }
+
 gulp.task('js',()=>{
     return gulp.src(path.js.input)
     .pipe(babel({
@@ -22,3 +26,13 @@ gulp.task('css',()=>{
     return gulp.src(path.css.input)
     .pipe(gulp.dest(path.css.output));
 });
+
+gulp.task('build',()=>{
+    return gulp.src(path.js.input)
+    .pipe(concat('common.min.js'))
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(path.js.output));
+})
